c105f03f8bc6789587d599c4997c4ed3dee2fc19
Emmanuel <emmaezele@yahoo.com>
2024-08-05 20:30:04 +0100

    cors

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv').config();
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/auth');
+const cors = require('cors'); 
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -11,9 +12,18 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
   
-  
+ 
+ app.use(cors());
+ 
+// Middleware setup
+app.use(cors({
+    origin: '*', // Adjust this to your specific origin as needed
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Origin', 'X-Requested-With', 'Accept'],
+  }));
 app.use(express.static('public'));
 
+  
 app.use(express.json());
 app.use('/api/auth', authRoutes);
 

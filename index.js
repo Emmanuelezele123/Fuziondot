c686946ba345285bbd66bc7d48604e085c4432d2
Emmanuel <emmaezele@yahoo.com>
2024-08-05 23:19:56 +0100

    Fixing cors

--- index.js
+++ index.js
@@ -17,15 +17,13 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 // Middleware setup
 app.use(express.static('public'));
 app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
 
 // Routes
 app.use('/api/auth', authRoutes);
 
 // Middleware setup
 app.use(cors({
-    origin: 'http://localhost:3000', // Adjust this to your specific origin as needed
+    origin: '*', // Adjust this to your specific origin as needed
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization', 'Origin', 'X-Requested-With', 'Accept'],
   }));

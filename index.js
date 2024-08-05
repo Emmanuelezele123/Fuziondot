a5656e917d8182a8bd1db82ed02edf0519b45dff
Emmanuel <emmaezele@yahoo.com>
2024-08-05 23:23:17 +0100

    cors fixing

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 3000;
+app.use(cors());
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
